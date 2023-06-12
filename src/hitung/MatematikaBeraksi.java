e58fd0942d873409ef0e8d6d95e4d79633eb53bb
IrfanSujarin <sujarinirfan@gmail.com>
2023-06-12 11:43:38 +0800

    Membuat class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -1,14 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package hitung;
 
-/**
- *
- * @author ASUS
- */
+import java.time.Clock;
+
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args) {
+        //membuat objek
+        Matematika irfan = new Matematika(2,0);
+        
+        System.out.println("Hasil Penjumlahan : "+irfan.setPenjumlahan());
+        System.out.println("Hasil Pengurangan : "+irfan.setPengurangan());
+        System.out.println("Hasil Perkalian : "+irfan.setPerkalian());
+        System.out.println("Hasil Pembagian : "+irfan.setPembagian());
+    }
 }
